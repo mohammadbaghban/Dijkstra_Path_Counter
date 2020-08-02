ba2397a3b546feb1858b6818f22e89ae6655685b
mohammadbaghban27 <mohammadbaghban27@gmail.com>
2020-08-02 16:04:29 +0430

    Dijkstra added from geeksforgeeks

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Dijkstra_Git
+{
+    internal class Program
+    {
+        private static int V; // Number of vertices
+
+        public static void Main(string[] args)
+        {
+        }
+
+        static int minDistance(int[] dist, bool[] sptSet, int V)
+        {
+            int min = int.MaxValue, min_index = -1;
+
+            for (int v = 0; v < V; v++)
+            {
+                if (sptSet[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+            }
+
+            return min_index;
+        }
+
+        static void dijkstra(int[,] graph, int src)
+        {
+            int[] dist = new int[V];
+
+            bool[] sptSet = new bool[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            dist[src] = 0;
+
+            for (int count = 0; count < V - 1; count++)
+            {
+                int u = minDistance(dist, sptSet, V);
+
+                sptSet[u] = true;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                    {
+                        dist[v] = dist[u] + graph[u, v];
+                    }
+                }
+            }
+        }
+    }
+}
